2cad642522facd46ca0f5415e1779dfc6eb66e8f
Boxuan_Wu <boxuan.wu@epfl.ch>
2025-10-08 01:37:57 +0200

    feat: add event screen UI + error & tag parsing logic
    Implement first look of add event screen  UI as
    Dialog and text field error logic
    Implement tag parsing logic to verify if user input
    tags are of correct format
    Add documentation to functions and composables

--- app/src/main/java/com/swent/mapin/MainActivity.kt
+++ app/src/main/java/com/swent/mapin/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.swent.mapin.ui.AddEventPopUp
 import com.swent.mapin.ui.theme.MapInTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +16,7 @@ class MainActivity : ComponentActivity() {
     setContent {
       MapInTheme {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          // Your app content goes here
+          AddEventPopUp {  }
         }
       }
     }
