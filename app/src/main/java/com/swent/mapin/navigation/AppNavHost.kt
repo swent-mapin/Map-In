8eeaf10843a2be2243fe9af9068613ab580cdcf2
bahri <nader.bahri@epfl.ch>
2025-10-09 16:17:26 +0200

    fix(map): update import path for MapScreen to reflect new package structure

--- app/src/main/java/com/swent/mapin/navigation/AppNavHost.kt
+++ app/src/main/java/com/swent/mapin/navigation/AppNavHost.kt
@@ -6,7 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.swent.mapin.authentication.AuthScreen
-import com.swent.mapin.map.MapScreen
+import com.swent.mapin.ui.map.MapScreen
 
 @Composable
 fun AppNavHost(
