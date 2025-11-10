d48524699dfd2051c07c3d0ca923823b1473ccbc
Boxuan_Wu <boxuan.wu@epfl.ch>
2025-11-10 18:40:49 +0100

    test: Add UI tests for pricing
    Add instrumentation tests for
    add event price

--- app/src/androidTest/java/com/swent/mapin/ui/event/addEvent/AddEventScreenTests.kt
+++ app/src/androidTest/java/com/swent/mapin/ui/event/addEvent/AddEventScreenTests.kt
@@ -69,6 +69,10 @@ class AddEventScreenTests {
         .performScrollTo()
         .assertIsDisplayed()
     composeTestRule
+        .onNodeWithTag(AddEventScreenTestTags.INPUT_EVENT_PRICE)
+        .performScrollTo()
+        .assertIsDisplayed()
+    composeTestRule
         .onNodeWithTag(AddEventScreenTestTags.INPUT_EVENT_LOCATION)
         .performScrollTo()
         .assertIsDisplayed()
@@ -220,6 +224,31 @@ class AddEventScreenTests {
   }
 
   @Test
+  fun validPriceInputWorks() {
+      composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_EVENT_TITLE).performTextInput("a")
+      composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_EVENT_TITLE).performTextClearance()
+      val tagNode = composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_EVENT_PRICE)
+      tagNode.assertIsDisplayed()
+      tagNode.performTextInput("InvalidPrice")
+      tagNode.performTextClearance()
+      tagNode.performTextInput("3.0")
+      composeTestRule
+          .onNodeWithTag(AddEventScreenTestTags.ERROR_MESSAGE)
+          .assert(!hasText("Price", substring = true, ignoreCase = true))
+  }
+  @Test
+  fun invalidPriceInputDoesNotWork() {
+      composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_EVENT_TITLE).performTextInput("a")
+      composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_EVENT_TITLE).performTextClearance()
+      val tagNode = composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_EVENT_PRICE)
+      tagNode.assertIsDisplayed()
+      tagNode.performTextInput("InvalidPrice")
+      composeTestRule
+          .onNodeWithTag(AddEventScreenTestTags.ERROR_MESSAGE)
+          .assert(hasText("Price", substring = true, ignoreCase = true))
+  }
+
+  @Test
   fun invalidInputsKeepSaveButtonDisabled() {
     composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_EVENT_TITLE).performTextInput("")
     composeTestRule
@@ -239,6 +268,7 @@ class SaveEventTests {
     val testDescription = "Some description"
     val testTags = listOf("tag1", "tag2")
     val isPublic = true
+    val price = 0.0
     val currentUserId = "FakeUserId"
 
     var onDoneCalled = false
@@ -253,7 +283,9 @@ class SaveEventTests {
         currentUserId = currentUserId,
         tags = testTags,
         isPublic = isPublic,
-        onDone = onDone)
+        onDone = onDone,
+        price = price
+    )
 
     verify { mockViewModel.addEvent(any<Event>()) }
     assert(onDoneCalled)
@@ -267,6 +299,7 @@ class SaveEventTests {
     val testDescription = "Some description"
     val testTags = listOf("tag1", "tag2")
     val isPublic = true
+    val price = 0.0
 
     val currentUserId: String? = null // simulate not logged in
 
@@ -282,7 +315,9 @@ class SaveEventTests {
         currentUserId = currentUserId,
         tags = testTags,
         isPublic = isPublic,
-        onDone = onDone)
+        onDone = onDone,
+        price = price
+    )
 
     verify(exactly = 0) { mockViewModel.addEvent(any<Event>()) } // should NOT be called
     assert(!onDoneCalled)
