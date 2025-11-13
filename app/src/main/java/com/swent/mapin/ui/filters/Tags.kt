500e73c6a9e53e033f57809b2ce6b28a6f9db231
Eloi <eloimarie.bernier@epfl.ch>
2025-11-13 16:20:13 +0100

    refactor(filters): Improve FilterSection
    - Add a tag class that contains all possible tags to be used in events,
     filters ect, instead of creating a list in FilterSection
    - Remove the pick on the map option for now as not implemented
    - Make the sections auto expand when toggled

---
+++ app/src/main/java/com/swent/mapin/ui/filters/Tags.kt
@@ -0,0 +1,24 @@
+package com.swent.mapin.ui.filters
+
+class Tags {
+  companion object {
+    val allTags = setOf(
+      "Music",
+      "Party",
+      "Sport",
+      "Food",
+      "Nature",
+      "Art",
+      "Tech",
+      "Dance",
+      "Cinema",
+      "Festival",
+      "Workshop",
+      "Club",
+      "Volunteering",
+      "Travel",
+      "Fitness",
+      "Board Games"
+    )
+  }
+}
