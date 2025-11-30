a1458b6e90db0483aa9df9295d0234426446c3cd
Boxuan_Wu <boxuan.wu@epfl.ch>
2025-11-30 19:56:50 +0100

    refactor: remove unused import
    Remove unused import in EventScreenComponentsTest.kt

--- app/src/androidTest/java/com/swent/mapin/ui/event/EventScreenComponentsTest.kt
+++ app/src/androidTest/java/com/swent/mapin/ui/event/EventScreenComponentsTest.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performScrollTo
 import androidx.compose.ui.test.performTextInput
 import com.swent.mapin.model.Location
 import com.swent.mapin.model.LocationViewModel
