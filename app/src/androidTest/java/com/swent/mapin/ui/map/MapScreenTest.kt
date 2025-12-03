b1608b2d7c5b6e2b6641811fc65a8bc6da7f6726
Chahd <chahdachkrou@gmail.com>
2025-12-03 17:29:30 +0100

    refactor: update test to assert existence of updated tab name in MapScreen

--- app/src/androidTest/java/com/swent/mapin/ui/map/MapScreenTest.kt
+++ app/src/androidTest/java/com/swent/mapin/ui/map/MapScreenTest.kt
@@ -43,7 +43,7 @@ class MapScreenTest {
   fun mapScreen_initialState_showsCollapsedSheet() {
     rule.setContent { MaterialTheme { MapScreen(autoRequestPermissions = false) } }
     rule.onNodeWithText("Search activities").assertIsDisplayed()
-    rule.onNodeWithText("Saved Events").assertExists()
+    rule.onNodeWithText("Saved").assertExists()
   }
 
   @Test
